9b278eeebc53e3c5d0e6ed5fa85bb4c2b5d60d1e
Mark Hallap <hallapmark@gmail.com>
2025-07-24 13:38:27 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mark Hallap on 2025-07-24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
