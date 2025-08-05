5b3002f54fcd32fd116dba6512e96141f33afc02
Mark Hallap <hallapmark@gmail.com>
2025-08-05 16:14:57 +0300

    Add attribution

--- GuessTheFlag/GuessTheFlagApp.swift
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -3,6 +3,9 @@
 //  GuessTheFlag
 //
 //  Created by Mark Hallap on 2025-07-24.
+
+// ---- Built following Paul Hudson's 100 Days of SwiftUI, with some enhancements. ----
+// https://www.hackingwithswift.com/books/ios-swiftui/guess-the-flag-introduction
 //
 
 import SwiftUI
