2b82aeeeb6846f17ea6ea680e201665f2333c0ac
Mark Hallap <hallapmark@gmail.com>
2025-09-14 09:54:06 +0300

    Add background colors as assets

--- GuessTheFlag/Views/GameView/BackgroundGradient/BackgroundGradient.swift
+++ GuessTheFlag/Views/GameView/BackgroundGradient/BackgroundGradient.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct BackgroundGradient: View {
     var body: some View {
         RadialGradient(stops: [
-            .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-            .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            .init(color: Color(.backgroundBlue), location: 0.3),
+            .init(color: Color(.backgroundRed), location: 0.3)
         ], center: .top, startRadius: 200, endRadius: 700)
         .ignoresSafeArea()
     }
